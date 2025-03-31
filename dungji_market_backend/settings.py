a5b94e80f84137bf45dc14d2fb331c5be4d42597
crom <holyfavor@gmail.com>
2025-03-31 17:56:20 +0900

    chore: update settings.py

--- dungji_market_backend/settings.py
+++ dungji_market_backend/settings.py
@@ -34,22 +34,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://13.125.227.190:8000",
 ]
 
-# CORS 설정
-CORS_ALLOWED_ORIGINS = os.getenv('CORS_ALLOWED_ORIGINS', '').split(',')
 CORS_ALLOW_CREDENTIALS = True
 CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', '').split(',')
-CORS_ALLOW_HEADERS = [
-    'accept',
-    'accept-encoding',
-    'authorization',
-    'content-type',
-    'dnt',
-    'origin',
-    'user-agent',
-    'x-csrftoken',
-    'x-requested-with',
-]
-
 # Application definition
 
 INSTALLED_APPS = [
