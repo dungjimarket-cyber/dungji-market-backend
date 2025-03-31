9e78e5765dc114066f9310475a37182c2e8f0978
crom <holyfavor@gmail.com>
2025-03-31 17:23:18 +0900

    chore: update Django settings configuration

--- dungji_market_backend/settings.py
+++ dungji_market_backend/settings.py
@@ -167,7 +167,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
